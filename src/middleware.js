b30de037c45fea93fb98cff1778a8fc340963234
Idba1 <moniraislamself@gmail.com>
2024-09-21 04:53:51 +0600

    check user cookie or token. if user have no token he/she willbe redirect login page

--- src/middleware.js
+++ src/middleware.js
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    const user = false; 
-    if (!user) {
+    const cookies = request.cookies.get('token')
+    if (!cookies) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
     return NextResponse.next();
