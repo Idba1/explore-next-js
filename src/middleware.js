2997dae1f663cf8041f77be67eabe0aa01a1f8a0
Idba1 <moniraislamself@gmail.com>
2024-09-21 04:28:11 +0600

    conditional middle ware

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server'
- 
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.rewrite(new URL('/blog/specialblog', request.url))
+    if (request.nextUrl.pathname.startsWith('/about')) {
+        return NextResponse.rewrite(new URL('/blog/specialblog', request.url))
+    }
 }
- 
-export const config = {
-  matcher: '/blog',
-}
+
+// export const config = {
+//   matcher: '/blog',
+// }
