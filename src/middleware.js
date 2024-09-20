be544557755ea8fce8d4fc63603029a9b0db75c6
Idba1 <moniraislamself@gmail.com>
2024-09-21 05:17:16 +0600

    code from 76_10 module. alcode run inn module but not work in my side. just save for next step

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,19 @@
 import { NextResponse } from 'next/server';
 
+const co = 'hello';
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    const cookies = request.cookies.get('token')
-    if (!cookies) {
+    console.log(request.cookies.get('token'));
+
+    const cookies = request.cookies.get('token');
+
+    if (!cookies || cookies.value !== co) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
     return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/blog',
+    matcher: '/blog',
 };
